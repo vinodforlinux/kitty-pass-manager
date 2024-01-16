0da313ba699111d652fc855f3f978825756ecfed
vinodforlinux <42178141+vinodforlinux@users.noreply.github.com>
2024-01-16 17:40:42 +0530

    Update pass.sh

--- pass.sh
+++ pass.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-selected_password=$(find ~/.password-store -type f -exec sh -c 'echo "${1#~/.password-store/}"' _ {} \; | awk -F/ '{gsub(/\.gpg$/, "", $NF); print $1 "/" $NF}' | fzf)
+selected_password=$(find ~/.password-store -type f -name '*.gpg' -exec sh -c 'echo "${1#~/.password-store/}"' _ {} \; | awk -F/ '{gsub(/\.gpg$/, "", $NF); print $1 "/" $NF}' | fzf)
 
 if [ -n "$selected_password" ]; then
     pass -c "$selected_password"
