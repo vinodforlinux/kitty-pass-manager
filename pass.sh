7031a6edd03af01916e892718ff9b97e91f0244f
vinodforlinux <42178141+vinodforlinux@users.noreply.github.com>
2024-01-16 13:20:04 +0530

    Create pass.sh

---
+++ pass.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+selected_password=$(find ~/.password-store -type f -exec sh -c 'echo "${1#~/.password-store/}"' _ {} \; | awk -F/ '{gsub(/\.gpg$/, "", $NF); print $1 "/" $NF}' | fzf)
+
+if [ -n "$selected_password" ]; then
+    pass -c "$selected_password"
+    read -s -p "Press enter to close the terminal"
+fi
